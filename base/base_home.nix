c47f7b1e2bd32c522c161f4faafaf4f22df07652
Kyle Kwong <47125306+SYKwong@users.noreply.github.com>
2026-03-24 18:34:43 -0700

    Factor Hyprland out of configuration.nix (#5)
    * factor out and remove hyprland from configuration.nix
    
    * No longer using uwsm

--- base/base_home.nix
+++ base/base_home.nix
@@ -5,17 +5,12 @@
   home.homeDirectory = "/home/${username}";
   home.stateVersion = "25.11";
 
-  wayland.windowManager.hyprland.systemd.enable = false;
-
   programs.git.enable = true;
   programs.bash = {
     enable = true;
-    shellAliases = {
-      btw = "echo i use nixos, btw";
-    };
     profileExtra = ''
-      if uwsm check may-start && uwsm select; then
-	      exec uwsm start default
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec hyprland
       fi
     '';
   };
---
+++ modules/hyprland.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  
+  environment.systemPackages = with pkgs; [
+    hypridle
+    hyprpolkitagent
+    waybar
+    grim
+    slurp
+    kitty
+  ];
+}
