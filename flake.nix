0b14017e54e13acf479b352c47aee9b149bda953
Kyle Kwong <kylekwong1@gmail.com>
2026-03-25 14:48:31 -0700

    enable iwd, which is used by impala

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
         disko.nixosModules.disko
         nixos-hardware.nixosModules.framework-16-7040-amd
 
-        { networking.wireless.enable = true; }
 
         home-manager.nixosModules.home-manager {
           home-manager = {
--- modules/wireless.nix
+++ modules/wireless.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  networking.wireless.enable = true;
+  networking.wireless.iwd.enable = true;
+  
   environment.systemPackages = with pkgs; [
     impala
     bluetui
