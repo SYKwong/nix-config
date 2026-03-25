19fb82a2e482de1b696bc51a4ab24676e6853123
Kyle Kwong <kylekwong1@gmail.com>
2026-03-25 14:50:50 -0700

    when using iwd, the normal wireless cannot be enabled

--- base/configuration.nix
+++ base/configuration.nix
@@ -15,7 +15,6 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  networking.wireless.enable = true; 
 
   time.timeZone = "America/Los_Angeles";
 
--- modules/wireless.nix
+++ modules/wireless.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 
 {
-  networking.wireless.enable = true;
   networking.wireless.iwd.enable = true;
   
   environment.systemPackages = with pkgs; [
