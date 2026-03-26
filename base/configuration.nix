4bd73bd5f1c10c83ecb37e9e642fad7ed4b90f33
Kyle Kwong <47125306+SYKwong@users.noreply.github.com>
2026-03-25 22:27:41 -0700

    Silent boot (#10)

--- base/configuration.nix
+++ base/configuration.nix
@@ -3,6 +3,28 @@
 {
   boot.initrd.systemd.enable = true;
   boot.loader.systemd-boot.enable = true;
+ 
+### For Silent Boot 
+### Silent Power Down Cannot Be Done Because of initrd 
+  boot = {
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "nowatchdog"
+      "systemd.show_status=false"
+      "vt.global_cursor_default=0"
+    ];
+    loader.timeout = 5;
+    plymouth.enable = true;
+    blacklistedKernelModules = [ "sp5100_tco" "iTCO_wdt" ];
+  };
+###
 
   services.getty.autologinUser = "${username}";
 
