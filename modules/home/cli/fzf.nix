d563bb19b23305455ffb6c5f4d2d05e7e50e279f
Kyle Kwong <kylekwong1@gmail.com>
2026-03-26 20:59:15 -0700

    add fzf

---
+++ modules/home/cli/fzf.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    colors = lib.mkForce {
+      "fg+" = accent;
+      "bg+" = "-1";
+      "fg" = foreground;
+      "bg" = "-1";
+      "prompt" = muted;
+      "pointer" = accent;
+    };
+    defaultOptions = [
+      "--margin=1"
+      "--layout=reverse"
+      "--border=none"
+      "--info='hidden'"
+      "--header=''"
+      "--prompt='/ '"
+      "-i"
+      "--no-bold"
+      "--bind='enter:execute(nvim {})'"
+      "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+      "--preview-window=right:60%:wrap"
+    ];
+  };
+}
--- modules/home/cli/system_packages.nix
+++ modules/home/cli/system_packages.nix
@@ -2,14 +2,9 @@
 
 {
   home.packages = with pkgs; [
-    fzf
     neovim
     wget
     wiremix
-    zoxide
   ];
 
-  programs = {
-    bat.enable = true;
-  };
 }
