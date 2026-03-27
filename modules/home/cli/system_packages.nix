ed807dc35a38f9f6224bb587912d2e67d48247b3
Kyle Kwong <kylekwong1@gmail.com>
2026-03-26 19:25:50 -0700

    home manager needs to use home.packages instead of environment.systemPackages

--- modules/home/cli/system_packages.nix
+++ modules/home/cli/system_packages.nix
@@ -1,7 +1,7 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, username, ... }:
 
 {
-   environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     fzf
     neovim
     tre-command
