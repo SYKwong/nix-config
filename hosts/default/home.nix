a0450e93dd2cdf5ef65f25711078bcc1efedc786
Kyle Kwong <kylekwong1@gmail.com>
2026-03-28 12:27:04 -0700

    use home manager to manage the config files

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,6 +6,10 @@
   ];
 
   wayland.windowManager.hyprland.systemd.enable = false;
+  
+  home.file.".config/hypr".source = ./../../config/hypr;
+  home.file.".config/waybar".source = ./../../config/waybar;
+
 
   programs.bash = {
     enable = true;
