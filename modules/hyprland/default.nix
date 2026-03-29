796a55fdcabc49a01c28456bb4ada90747a063d0
Kyle Kwong <kylekwong1@gmail.com>
2026-03-29 02:51:28 -0700

    rofi can only be a systempkg

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -4,7 +4,6 @@
   imports = [
     ./hyprland.nix
     ./hyprlock.nix
-    ./rofi.nix
   ];
 
   environment.systemPackages = with pkgs; [
@@ -14,5 +13,6 @@
     grim
     slurp
     kitty
+    rofi
   ];
 }
