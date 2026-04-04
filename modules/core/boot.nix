e00ddb6bce62767c0b66b6e027a4cb8e18b97fc3
Kyle Kwong <kylekwong1@gmail.com>
2026-04-04 01:18:53 -0700

    force disable grub

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -4,6 +4,7 @@
   boot = {
     initrd.systemd.enable = true;
     loader.systemd-boot.enable = true;
+    loader.grub.enable = false;
   };
 ### For Silent Boot 
 ### Silent Power Down Cannot Be Done Because of initrd 
