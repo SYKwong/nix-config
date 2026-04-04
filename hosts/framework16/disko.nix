0ddf6d751f4367e63535c8fa2ba1d003b2e63446
Kyle Kwong <kylekwong1@gmail.com>
2026-04-04 01:32:04 -0700

    use old disko file

--- hosts/framework16/disko.nix
+++ hosts/framework16/disko.nix
@@ -1,58 +1,67 @@
 {
   disko.devices = {
-    nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = [
-        "size=8G"
-        "defaults"
-        "mode=755"
-      ];
-    };
-
-    disk.main = {
-      type = "disk";
-      device = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_4TB_S7KGNJ0W903445L";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "2G";
-            type = "EF00";
-            content = { type = "filesystem"; format = "vfat"; mountpoint = "/boot"; mountOptions = [ "umask=0077" ]; };
-          };
-          luks = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "crypted";
-              passwordFile = "/tmp/secret.key";
-              content = { type = "lvm_pv"; vg = "pool"; };
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_4TB_S7KGNJ0W903445L";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "2G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
             };
-          };
-        };
-      };
-    };
-    lvm_vg.pool = {
-      type = "lvm_vg";
-      lvs = {
-        swap = {
-          size = "100G";
-          content = { type = "swap"; resumeDevice = true; };
-        };
-        storage = {
-          size = "100%FREE";
-          content = {
-            type = "btrfs";
-            subvolumes = {
-              "/nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
-              "/persist" = { mountpoint = "/persist"; mountOptions = [ "compress=zstd" "noatime" ]; };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                };
+                passwordFile = "/tmp/secret.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "100G";
+                    };
+                  };
+                };
+              };
             };
           };
         };
       };
     };
   };
-
-  fileSystems."/nix".neededForBoot = true;
-  fileSystems."/persist".neededForBoot = true;
 }
