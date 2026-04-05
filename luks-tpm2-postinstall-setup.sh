a31edede6816ff235a7da8dddea9f8c95145ffb1
Kyle Kwong <kylekwong1@gmail.com>
2026-04-04 20:48:14 -0700

    remove the post install script

--- luks-tpm2-postinstall-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo sbctl enroll-keys --microsoft
-sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 /dev/disk/by-partlabel/disk-main-luks
-echo "reboot and enable secure boot in bios"
