2b6336378c5e2b9075299ad2133339e1a7d149ab
Kyle Kwong <kylekwong1@gmail.com>
2026-04-05 00:56:00 -0700

    add cache

--- overlays/cachyos-kernel.nix
+++ overlays/cachyos-kernel.nix
@@ -4,4 +4,8 @@
   nixpkgs.overlays = [
     inputs.nix-cachyos-kernel.overlays.default
   ];
+
+  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+
 }
