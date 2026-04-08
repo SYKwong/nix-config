9d001e805058854336fd4e34f0f2c908a51bc70c
Kyle Kwong <kylekwong1@gmail.com>
2026-04-07 22:03:41 -0700

    test

--- modules/shell-scripts/power-menu.nix
+++ modules/shell-scripts/power-menu.nix
@@ -3,24 +3,19 @@
 let
   power-menu = pkgs.writeShellScriptBin "power-menu" ''
     current=$(powerprofilesctl get)
-
-# Define the options
     options="performance\nbalanced\npower-saver"
-
-# Rofi command
-# We use -mesg to show the current active profile at the top
-    chosen=$(echo -e "$options" | rofi -dmenu \
+   
+    chosen=$(echo -e "''$options" | rofi -dmenu \
       -i \
       -p "Power Profile" \
-      -mesg "Current: $current" \
+      -mesg "Current: ''$current" \
       -theme-str 'window {width: 20em;} listview {lines: 3;}')
 
 # Apply the profile if a choice was made
-    if [ -n "$chosen" ]; then
-      powerprofilesctl set "$chosen"
-      notify-send "Power Profile" "Switched to $chosen" -i "battery-good"
+    if [[ -n "''$chosen" ]]; then
+      powerprofilesctl set "''$chosen"
+      notify-send "Power Profile" "Switched to ''$chosen" -i "battery-good"
     fi
-
   '';
 in
 {
