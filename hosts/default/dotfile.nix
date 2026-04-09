de61641855011c4a8f3ca4da5ef001e8258feeb8
Kyle Kwong <kylekwong1@gmail.com>
2026-04-08 19:35:39 -0700

    try out of store symnlink

--- hosts/default/dotfile.nix
+++ hosts/default/dotfile.nix
@@ -1,33 +1,33 @@
-{ config, hostname, ... }:
+{ config, hostname, username, ... }:
 let
-  config_path = "$HOME/nix-config/config";
+  config_path = "/home/${username}/nix-config/config";
+  symlink = path: config.lib.file.mkOutOfStoreSymlink "${config_path}/${path}";
 in 
 {
   # UWSM quirk with systemd 
   wayland.windowManager.hyprland.systemd.enable = false;
   
-  xdg.configFile."hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hyprland/hosts/${hostname}.conf";
+  xdg.configFile."hypr/hyprland.conf".source = symlink "hypr/hyprland/hosts/${hostname}.conf"; 
+  xdg.configFile."hypr/hyprland/common".source = symlink "hypr/hyprland/common";
 
-  xdg.configFile."hypr/hyprland/common".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hyprland/common";
+  xdg.configFile."hypr/hypridle.conf".source = symlink "hypr/hypridle/hosts/${hostname}.conf";
+  xdg.configFile."hypr/hypridle/common".source = symlink "hypr/hypridle/common";
 
-  xdg.configFile."hypr/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hypridle/hosts/${hostname}.conf";
-  xdg.configFile."hypr/hypridle/common".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hypridle/common";
+  xdg.configFile."hypr/hyprlock.conf".source = symlink "hypr/hyprlock/hyprlock.conf" ;
 
-  xdg.configFile."hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hyprlock/hyprlock.conf" ;
+  xdg.configFile."hypr/hyprpaper.conf".source = symlink "hypr/hyprpaper/hyprpaper.conf";
+  xdg.configFile."hypr/wallpaper".source = symlink "hypr/hyprpaper/wallpaper";
 
-  xdg.configFile."hypr/hyprpaper.conf".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hyprpaper/hyprpaper.conf";
-  xdg.configFile."hypr/wallpaper".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/hypr/hyprpaper/wallpaper";
+  xdg.configFile."niri/config.kdl".source = symlink "niri/hosts/${hostname}.kdl";
+  xdg.configFile."niri/animation.kdl".source = symlink "niri/common/animation.kdl";
+  xdg.configFile."niri/auto-start.kdl".source = symlink "niri/common/auto-start.kdl";
+  xdg.configFile."niri/input.kdl".source = symlink "niri/common/input.kdl";
+  xdg.configFile."niri/keybind.kdl".source = symlink "niri/common/keybind.kdl";
+  xdg.configFile."niri/layout.kdl".source = symlink "niri/common/layout.kdl";
+  xdg.configFile."niri/misc.kdl".source = symlink "niri/common/misc.kdl";
+  xdg.configFile."niri/window-rule.kdl".source = symlink "niri/common/window-rule.kdl";
 
-  xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/hosts/${hostname}.kdl";
-  xdg.configFile."niri/animation.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/animation.kdl";
-  xdg.configFile."niri/auto-start.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/auto-start.kdl";
-  xdg.configFile."niri/input.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/input.kdl";
-  xdg.configFile."niri/keybind.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/keybind.kdl";
-  xdg.configFile."niri/layout.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/layout.kdl";
-  xdg.configFile."niri/misc.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/misc.kdl";
-  xdg.configFile."niri/window-rule.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/niri/common/window-rule.kdl";
+  xdg.configFile."waybar".source = symlink "waybar";
 
-  xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/waybar";
-
-  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "${config_path}/rofi";
+  xdg.configFile."rofi".source = symlink "rofi";
 }
