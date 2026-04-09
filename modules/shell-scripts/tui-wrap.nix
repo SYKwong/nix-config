ac30e2a879ffe40651058fc25af1e550136f8cd4
Kyle Kwong <kylekwong1@gmail.com>
2026-04-09 16:56:00 -0700

    test

--- modules/shell-scripts/tui-wrap.nix
+++ modules/shell-scripts/tui-wrap.nix
@@ -12,24 +12,23 @@ let
     }
 
     focus_and_warp() {
-      local id=$1
-      hyprctl dispatch focuswindow "class:$id"
+      hyprctl dispatch focuswindow "class:$APP_ID"
       hyprctl dispatch alterzorder top
     }
 
-    EXISTING_ID=$(get_window)
+    ALREADY_EXIST=$(get_window)
 
-    if [ -n "$EXISTING_ID" ] && [ "$EXISTING_ID" != "null" ]; then
-      focus_and_warp "$EXISTING_ID"
+    if [ -n "$ALREADY_EXIST" ] && [ "$ALREADY_EXIST" != "null" ]; then
+      focus_and_warp
       exit 0
     fi
 
     exec setsid uwsm-app -- xdg-terminal-exec --app-id="$APP_ID" -e "$APP_NAME" "$@"
 
     for i in {1..20}; do # Try for 2 seconds (20 * 0.1s)
-      NEW_ID=$(get_window)
-      if [ -n "$NEW_ID" ] && [ "$NEW_ID" != "null" ]; then
-        focus_and_warp "$NEW_ID"
+      NEW_INSTANCE=$(get_window)
+      if [ -n "$NEW_INSTANCE" ] && [ "$NEW_INSTANCE" != "null" ]; then
+        focus_and_warp 
         exit 0
       fi
       sleep 0.1
