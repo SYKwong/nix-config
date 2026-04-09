a38a183a66fddbe0b07b1ab709a67ef86c32d597
Kyle Kwong <kylekwong1@gmail.com>
2026-04-09 14:51:56 -0700

    try to always put cursor on the window

--- modules/shell-scripts/tui-wrap.nix
+++ modules/shell-scripts/tui-wrap.nix
@@ -4,14 +4,36 @@ let
   tuiWrap = pkgs.writeShellScriptBin "tui-wrap" ''
     APP_NAME=$1
     shift
+    
+    APP_ID="tui-float.$APP_NAME"
+    
+    get_window(){
+      hyprctl clients | grep -q "tui-float.$APP_NAME"
+    }
 
-    if hyprctl clients | grep -q "tui-float.$APP_NAME"; then
-      hyprctl dispatch focuswindow "class:tui-float.$APP_NAME"
+    focus_and_warp() {
+      hyprctl dispatch focuswindow "class:$APP_ID"
       hyprctl dispatch alterzorder top
+    }
+
+    EXISTING_ID=$(get_window)
+
+    if [ -n "$EXISTING_ID" ] && [ "$EXISTING_ID" != "null" ]; then
+      focus_and_warp "$EXISTING_ID"
       exit 0
     fi
 
     exec setsid uwsm-app -- xdg-terminal-exec --app-id="tui-float.$APP_NAME" -e "$APP_NAME" "$@"
+
+    for i in {1..20}; do # Try for 2 seconds (20 * 0.1s)
+      NEW_ID=$(get_window)
+      if [ -n "$NEW_ID" ] && [ "$NEW_ID" != "null" ]; then
+        focus_and_warp "$NEW_ID"
+        exit 0
+      fi
+      sleep 0.1
+    done
+
   '';
 in
 {
