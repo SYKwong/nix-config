543bef86c1de162d5c1c929389a8c967a7f4b3a7
Kyle Kwong <kylekwong1@gmail.com>
2026-04-08 17:36:57 -0700

    see if this makes config update faster

--- hosts/default/dotfile.nix
+++ hosts/default/dotfile.nix
@@ -1,30 +1,30 @@
-{ hostname, ... }:
+{ config, hostname, ... }:
 
 {
   # UWSM quirk with systemd 
   wayland.windowManager.hyprland.systemd.enable = false;
   
-  xdg.configFile."hypr/hyprland.conf".source = ./../../config/hypr/hyprland/hosts/${hostname}.conf;
-  xdg.configFile."hypr/hyprland/common".source = ./../../config/hypr/hyprland/common;
+  xdg.configFile."hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hyprland/hosts/${hostname}.conf;
+  xdg.configFile."hypr/hyprland/common".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hyprland/common;
 
-  xdg.configFile."hypr/hypridle.conf".source = ./../../config/hypr/hypridle/hosts/${hostname}.conf;
-  xdg.configFile."hypr/hypridle/common".source = ./../../config/hypr/hypridle/common;
+  xdg.configFile."hypr/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hypridle/hosts/${hostname}.conf;
+  xdg.configFile."hypr/hypridle/common".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hypridle/common;
 
-  xdg.configFile."hypr/hyprlock.conf".source = ./../../config/hypr/hyprlock/hyprlock.conf;
+  xdg.configFile."hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hyprlock/hyprlock.conf;
 
-  xdg.configFile."hypr/hyprpaper.conf".source = ./../../config/hypr/hyprpaper/hyprpaper.conf;
-  xdg.configFile."hypr/wallpaper".source = ./../../config/hypr/hyprpaper/wallpaper;
+  xdg.configFile."hypr/hyprpaper.conf".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hyprpaper/hyprpaper.conf;
+  xdg.configFile."hypr/wallpaper".source = config.lib.file.mkOutOfStoreSymlink ./../../config/hypr/hyprpaper/wallpaper;
 
-  xdg.configFile."niri/config.kdl".source = ./../../config/niri/hosts/${hostname}.kdl;
-  xdg.configFile."niri/animation.kdl".source = ./../../config/niri/common/animation.kdl;
-  xdg.configFile."niri/auto-start.kdl".source = ./../../config/niri/common/auto-start.kdl;
-  xdg.configFile."niri/input.kdl".source = ./../../config/niri/common/input.kdl;
-  xdg.configFile."niri/keybind.kdl".source = ./../../config/niri/common/keybind.kdl;
-  xdg.configFile."niri/layout.kdl".source = ./../../config/niri/common/layout.kdl;
-  xdg.configFile."niri/misc.kdl".source = ./../../config/niri/common/misc.kdl;
-  xdg.configFile."niri/window-rule.kdl".source = ./../../config/niri/common/window-rule.kdl;
+  xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/hosts/${hostname}.kdl;
+  xdg.configFile."niri/animation.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/animation.kdl;
+  xdg.configFile."niri/auto-start.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/auto-start.kdl;
+  xdg.configFile."niri/input.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/input.kdl;
+  xdg.configFile."niri/keybind.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/keybind.kdl;
+  xdg.configFile."niri/layout.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/layout.kdl;
+  xdg.configFile."niri/misc.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/misc.kdl;
+  xdg.configFile."niri/window-rule.kdl".source = config.lib.file.mkOutOfStoreSymlink ./../../config/niri/common/window-rule.kdl;
 
-  xdg.configFile."waybar".source = ./../../config/waybar;
+  xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink ./../../config/waybar;
 
-  xdg.configFile."rofi".source = ./../../config/rofi;
+  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink ./../../config/rofi;
 }
