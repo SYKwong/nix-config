4153d8a9e3f8d64bd7d9bdd74479daef977c94e2
Kyle Kwong <kylekwong1@gmail.com>
2026-04-09 04:58:45 -0700

    this should avoid creating multiple tui wrappers of the same app

--- modules/shell-scripts/tui-wrap.nix
+++ modules/shell-scripts/tui-wrap.nix
@@ -5,6 +5,11 @@ let
     APP_NAME=$1
     shift
 
+    if hyprctl clients | grep -q "tui-float.$APP_NAME"; then
+      hyprctl dispatch focuswindow "class:tui-float.$APP_NAME"
+      exit 0
+    fi
+
     exec setsid uwsm-app -- xdg-terminal-exec --app-id="tui-float.$APP_NAME" -e "$APP_NAME" "$@"
   '';
 in
