48dba75e589b69a87b4255f8d84a0310d1e578ac
Kyle Kwong <kylekwong1@gmail.com>
2026-04-11 15:07:29 -0700

    -C path needs to be before the command

--- hosts/default/alias.nix
+++ hosts/default/alias.nix
@@ -2,7 +2,7 @@
 
 {
   home.shellAliases = {
-    npull = "git pull -C ~/nix-config";
+    npull = "git -C ~/nix-config pull";
     nrs = "sudo nixos-rebuild switch --flake ~/nix-config/#${hostname}";
     nrb = "sudo nixos-rebuild boot --flake ~/nix-config/#${hostname}";
   };
