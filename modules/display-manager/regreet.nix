343b2c5d40c01d74df912a74c1ea7675ed9c5340
Kyle Kwong <kylekwong1@gmail.com>
2026-04-12 19:19:09 -0700

    greetd seems to be enabled by defualt

--- modules/display-manager/regreet.nix
+++ modules/display-manager/regreet.nix
@@ -2,19 +2,5 @@
 
 {
   programs.regreet.enable = true;
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      initial_session = {
-        command = "${session-command} &>/dev/null";
-        user = "${username}";
-      };
-      default_session = {
-        command = "${session-command} &>/dev/null";
-        user = "${username}";
-      };
-    };
-  };
 }
 
