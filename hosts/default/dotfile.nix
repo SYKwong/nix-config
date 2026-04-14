b1b90b42dea39efc1c2f3a74c03088e3ca872710
Kyle Kwong <kylekwong1@gmail.com>
2026-04-14 14:27:12 -0700

    add theme to dotfile

--- hosts/default/dotfile.nix
+++ hosts/default/dotfile.nix
@@ -4,22 +4,23 @@ let
   symlink = path: config.lib.file.mkOutOfStoreSymlink "${config_path}/${path}";
 
   files = {
-    "hypr/hyprland.conf"   = "hypr/hyprland/hosts/${hostname}.conf";
-    "hypr/hyprland/common" = "hypr/hyprland/common";
-    "hypr/hypridle.conf"   = "hypr/hypridle/hosts/${hostname}.conf";
-    "hypr/hypridle/common" = "hypr/hypridle/common";
-    "hypr/hyprlock.conf"   = "hypr/hyprlock/hyprlock.conf";
-    "hypr/hyprpaper.conf"  = "hypr/hyprpaper/hyprpaper.conf";
-    "hypr/wallpaper"       = "hypr/hyprpaper/wallpaper";
+    "hypr/hyprland.conf"    = "hypr/hyprland/hosts/${hostname}.conf";
+    "hypr/hyprland/common"  = "hypr/hyprland/common";
+    "hypr/hypridle.conf"    = "hypr/hypridle/hosts/${hostname}.conf";
+    "hypr/hypridle/common"  = "hypr/hypridle/common";
+    "hypr/hyprlock.conf"    = "hypr/hyprlock/hyprlock.conf";
+    "hypr/hyprpaper.conf"   = "hypr/hyprpaper/hyprpaper.conf";
+    "hypr/wallpaper"        = "hypr/hyprpaper/wallpaper";
+    "hypr/mocha.conf"       = "hype/mocha.conf"
 
-    "niri/config.kdl"      = "niri/hosts/${hostname}.kdl";
-    "niri/animation.kdl"   = "niri/common/animation.kdl";
-    "niri/auto-start.kdl"  = "niri/common/auto-start.kdl";
-    "niri/input.kdl"       = "niri/common/input.kdl";
-    "niri/keybind.kdl"     = "niri/common/keybind.kdl";
-    "niri/layout.kdl"      = "niri/common/layout.kdl";
-    "niri/misc.kdl"        = "niri/common/misc.kdl";
-    "niri/window-rule.kdl" = "niri/common/window-rule.kdl";
+    "niri/config.kdl"       = "niri/hosts/${hostname}.kdl";
+    "niri/animation.kdl"    = "niri/common/animation.kdl";
+    "niri/auto-start.kdl"   = "niri/common/auto-start.kdl";
+    "niri/input.kdl"        = "niri/common/input.kdl";
+    "niri/keybind.kdl"      = "niri/common/keybind.kdl";
+    "niri/layout.kdl"       = "niri/common/layout.kdl";
+    "niri/misc.kdl"         = "niri/common/misc.kdl";
+    "niri/window-rule.kdl"  = "niri/common/window-rule.kdl";
 
     "waybar"  = "waybar";
     "rofi"    = "rofi";
