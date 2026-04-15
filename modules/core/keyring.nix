49b74023d63ba8e54f525bc0edc4e2b30c13d9dd
Kyle Kwong <kylekwong1@gmail.com>
2026-04-15 16:52:40 -0700

    add systemd service

--- modules/core/keyring.nix
+++ modules/core/keyring.nix
@@ -1,5 +1,18 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.hyprlock.enableGnomeKeyring = lib.mkIf config.programs.hyprlock.enable true;
+  systemd.user.services.gnome-keyring = {
+    description = "GNOME Keyring";
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "org.freedesktop.secrets";
+      ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --foreground --components=secrets";
+      #ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --foreground --components=secrets";
+    Restart = "on-abort";
+    };
+    wantedBy = [ "graphical-session.target" ];
+  };
 }
