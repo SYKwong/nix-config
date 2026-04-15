eafd845b3edc7765796ddd14285af61ff68a72b7
Kyle Kwong <kylekwong1@gmail.com>
2026-04-14 22:41:44 -0700

    add pam service

--- modules/core/keyring.nix
+++ modules/core/keyring.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.hyprlock.enableGnomeKeyring = lib.mkIf config.programs.hyprlock.enable true;
