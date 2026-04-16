c700288a90a5c5b259bb632887872f2ce0704ce3
Kyle Kwong <kylekwong1@gmail.com>
2026-04-16 13:11:31 -0700

    add systemd service

--- modules/core/keyring.nix
+++ modules/core/keyring.nix
@@ -1,18 +1,32 @@
 { pkgs, lib, config, ... }:
 {
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.hyprlock.enableGnomeKeyring = lib.mkIf config.programs.hyprlock.enable true;
-  systemd.user.services.gnome-keyring = {
-    description = "GNOME Keyring";
-    partOf = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "dbus";
-      BusName = "org.freedesktop.secrets";
-      ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --foreground --components=secrets";
-      #ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --foreground --components=secrets";
-    Restart = "on-abort";
-    };
-    wantedBy = [ "graphical-session.target" ];
+  services.gnome = {
+    gnome-keyring.enable = true;
   };
+    #security.pam.services.hyprlock.enableGnomeKeyring = lib.mkIf config.programs.hyprlock.enable true;
+  
+  security.pam.services = {
+    greetd.enableGnomeKeyring = true;
+    greetd-password.enableGnomeKeyring = true;
+    hyprlock.enableGnomeKeyring = true;
+    login.enableGnomeKeyring = true;
+  };
+  services.dbus.packages = [ pkgs.gnome-keyring pkgs.gcr ];
+
+  systemd.user.services = {
+    hyprpolkitagent = {
+      description = "Hyprpolkit authentication agent";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
 }
