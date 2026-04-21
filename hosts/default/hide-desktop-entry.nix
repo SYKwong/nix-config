5b86e744caa83f62ffc4993c1727a25074e0f9d1
Kyle Kwong <kylekwong1@gmail.com>
2026-04-20 23:29:57 -0700

    Revert "hide desktop entries"
    This reverts commit 196728d9aba77c70d5ab82e2834e2aa4191ec7aa.

--- hosts/default/hide-desktop-entry.nix
+++ hosts/default/hide-desktop-entry.nix
@@ -1,19 +1,7 @@
 { lib, ... }:
 
 let
-  appsToHide = [
-    # Foot is only used for TUI app
-    "foot"
-    "foot-server"
-    "footclient"
-
-    # Installed as Stylix dependency
-    "qt5ct"
-    "qt6ct"
-
-    # Misc
-    "nixos-manual"
-    "rofi-theme-selector" 
+  appsToHide = [   
   ];
 in
 {
