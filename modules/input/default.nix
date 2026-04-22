5b5454239e8d511c678780d4d9c8b33126409ed5
Kyle Kwong <kylekwong1@gmail.com>
2026-04-21 18:32:19 -0700

    add fcitx5 to config

---
+++ modules/input/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+
+{
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+
+    fcitx5 = {
+      waylandFrontend = true;
+      ignoreUserConfig = true; # Required for using the following settings
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-chinese-addons
+        fcitx5-table-extra
+      ];
+
+      settings = {
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "quick-classic";
+          "Groups/0/Items/2".Name = "mozc";
+        };
+
+        addons = {
+          classicui = {
+            Theme = "default-dark";
+            VerticalCandidateList = "False";
+            Font = "Sans 15";
+            MenuFont="Sans 15";
+            TrayFont="Sans Bold 15";
+          };
+
+          punctuation = {
+            Enabled = "False";
+          };
+        };
+      };
+    };
+  };
+}
