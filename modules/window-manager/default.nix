013b92363a14ba8e1f4719fa2c38ec2fadb1933a
fw16-kyle <fw16-kyle@example.com>
2026-05-01 16:47:38 -0700

    refactor niri too

--- modules/window-manager/default.nix
+++ modules/window-manager/default.nix
@@ -2,5 +2,6 @@
   imports = [ 
     ./common
     ./hyprland.nix
+    ./niri.nix
   ];
 }
---
+++ modules/window-manager/niri.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, window-manager, ... }:
+
+{
+  imports = [ ./common ];
+  
+  config = lib.mkIf (window-manager == "niri"){
+
+    _module.args.session-command = "niri-session";
+
+    programs.niri.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      alacritty
+      xwayland-satellite
+    ];
+    
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+      configPackages = [ pkgs.niri ];
+    };
+  
+  };
+}
+
