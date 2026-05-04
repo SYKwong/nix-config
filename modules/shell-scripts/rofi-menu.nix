576c85054814ec1b64dc5d21ee21fb336703ad8f
fw16-kyle <fw16-kyle@example.com>
2026-05-04 12:33:00 -0700

    rofi power menu(shutdown, reboot, etc.

--- modules/shell-scripts/rofi-menu.nix
+++ modules/shell-scripts/rofi-menu.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 let
-  rofi-power-menu = pkgs.writeShellScriptBin "rofi-power-menu" ''
+  rofi-power-options = pkgs.writeShellScriptBin "rofi-power-options" ''
     current=$(powerprofilesctl get)
     options="performance\nbalanced\npower-saver"
    
@@ -11,13 +11,37 @@ let
       -mesg "Current: ''$current" \
       -theme-str 'window {width: 20em;} listview {lines: 3;}')
 
-# Apply the profile if a choice was made
     if [[ -n "''$chosen" ]]; then
       powerprofilesctl set "''$chosen"
       notify-send "Power Profile" "Switched to ''$chosen" -i "battery-good"
     fi
   '';
+
+  rofi-power-menu = pkgs.writeShellScriptBin "rofi-power-menu" ''
+    options="Shutdown\nSuspend\nHibernate\nReboot\nLog Out\nLock"
+
+    chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu:")
+    case "$chosen" in
+      "Shutdown")
+        systemctl poweroff ;;
+      "Suspend")
+        systemctl suspend-then-hibernate ;;
+      "Hibernate")
+        systemctl hibernate ;;
+      "Reboot")
+        systemctl reboot ;;
+      "Log Out")
+        loginctl terminate-user $USER ;;
+      "Lock")
+        pidof hyprlock || hyprlock ;; 
+      *)
+        exit 1 ;;
+    esac
+  '';
 in
 {
-  environment.systemPackages = [ rofi-power-menu ];
+  environment.systemPackages = [ 
+    rofi-power-options
+    rofi-power-menu
+  ];
 }
