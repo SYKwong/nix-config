3bd33b1d42a42d8d12c1173ba2f183e4127e5be8
fw16-kyle <fw16-kyle@example.com>
2026-05-03 21:18:28 -0700

    nvim tab size

--- modules/core/system_packages.nix
+++ modules/core/system_packages.nix
@@ -9,13 +9,19 @@
   ];
 
   programs = {
-	neovim = {
-	  enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-	  viAlias = true;
+	  neovim = {
+	    enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+	      viAlias = true;
+	      configure.customLuaRC = ''
+          vim.opt.tabstop = 2      -- Number of spaces that a <Tab> counts for
+          vim.opt.shiftwidth = 2   -- Size of an indent
+          vim.opt.expandtab = true -- Use spaces instead of tabs
+          vim.opt.softtabstop = 2  -- Number of spaces a <Tab> counts for while editing
+        '';
   	};
-	nano.enable = false;
+	  nano.enable = false;
   };
 
   xdg.terminal-exec = {
