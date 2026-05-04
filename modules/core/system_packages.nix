551df2eb4a3bc3d6da65a2b1b27deaef33e76b85
Kyle Kwong <47125306+SYKwong@users.noreply.github.com>
2026-05-03 21:19:44 -0700

    change default editor from nano to neovim (#51)

--- modules/core/system_packages.nix
+++ modules/core/system_packages.nix
@@ -3,11 +3,26 @@
 {
   environment.systemPackages = with pkgs; [
     kitty
-    neovim
     wget
     wiremix
     wl-clipboard
   ];
+
+  programs = {
+	  neovim = {
+	    enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+	      viAlias = true;
+	      configure.customLuaRC = ''
+          vim.opt.tabstop = 2      -- Number of spaces that a <Tab> counts for
+          vim.opt.shiftwidth = 2   -- Size of an indent
+          vim.opt.expandtab = true -- Use spaces instead of tabs
+          vim.opt.softtabstop = 2  -- Number of spaces a <Tab> counts for while editing
+        '';
+  	};
+	  nano.enable = false;
+  };
 
   xdg.terminal-exec = {
     enable = true;
