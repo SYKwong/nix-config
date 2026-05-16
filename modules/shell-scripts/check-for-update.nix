531e93c1f81962712c43572559d508cf61bc3d75
Kyle Kwong <kylekwong1@gmail.com>
2026-05-16 02:17:19 +0000

    add a retry logic to update-available to make sure it is less likely to not be...

--- modules/shell-scripts/check-for-update.nix
+++ modules/shell-scripts/check-for-update.nix
@@ -4,11 +4,32 @@ let
   config_path = "/home/${username}/nix-config";
 
   update-available = pkgs.writeShellScriptBin "update-available" ''
+    MAX_ATTEMPTS=3
+    ATTEMPT=1
+    DELAY_SEC=10
+
     REPO="${config_path}"
     local_hash=$(git -C "$REPO" rev-parse main 2>/dev/null)
-    remote_hash=$(git -C "$REPO" ls-remote origin -h refs/heads/main 2>/dev/null | awk '{print $1}')
+    remote_hash=""
 
-    if [[ -n "$remote_hash" && "$local_hash" != "$remote_hash" ]]; then
+    while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
+      remote_hash=$(git -C "$REPO" ls-remote origin -h refs/heads/main 2>/dev/null | awk '{print $1}')
+      
+      if [[ -n "$remote_hash" ]]; then
+        break
+      fi
+      
+      if [ $ATTEMPT -lt $MAX_ATTEMPTS ]; then
+        echo "Network error or timeout. Retrying in $DELAY seconds... ($ATTEMPT/$MAX_ATTEMPTS)" >&2
+        sleep $DELAY
+      fi
+      ((ATTEMPT++))
+    done
+
+    if [[ -z "$remote_hash" ]]; then
+      echo "Error: Could not connect to remote repository after $MAX_ATTEMPTS attempts." >&2
+      exit 2
+    elif [[ -n "$remote_hash" && "$local_hash" != "$remote_hash" ]]; then
       echo "update available $remote_hash"
       exit 0
     else
