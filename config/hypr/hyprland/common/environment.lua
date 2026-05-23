ea652f0db8481373c6c8edf374b545237d55ceb8
Kyle Kwong <kylekwong1@gmail.com>
2026-05-23 00:39:55 -0700

    gtk theme stuff

--- config/hypr/hyprland/common/environment.lua
+++ config/hypr/hyprland/common/environment.lua
@@ -1,7 +1,8 @@
 -- See https://wiki.hyprland.org/Configuring/Environment-variables/
 
---hl.env("GTK_THEME", "Tokyo-Night-Dark")
---hl.env("GTK_ICON_THEME", "Adwaita")
+hl.env("GTK_THEME", "Adwaita:dark")
+hl.env("GTK_ICON_THEME", "Adwaita")
+hl.env("GTK_CSD", "0")
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
