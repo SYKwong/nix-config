3406b6f923e2bdd7ce702e539ca22938122a0e9b
Kyle Kwong <kylekwong1@gmail.com>
2026-05-26 18:26:49 +0000

    Alias update

--- hosts/default/alias.nix
+++ hosts/default/alias.nix
@@ -3,8 +3,8 @@
 {
   home.shellAliases = {
     npull = "git -C ~/nix-config pull";
-    nrs = "sudo nixos-rebuild switch --flake ~/nix-config/#${hostname}";
-    nrb = "sudo nixos-rebuild boot --flake ~/nix-config/#${hostname}";
+    nrs = "nixos-rebuild switch --flake ~/nix-config/#${hostname}";
+    nrb = "nixos-rebuild boot --flake ~/nix-config/#${hostname}";
     
     n = "nvim"; 
   };
