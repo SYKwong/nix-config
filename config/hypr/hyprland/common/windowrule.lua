8cc7489c9fc6dc8082fca3866b624ac2aa2813ff
Kyle Kwong <kylekwong1@gmail.com>
2026-05-31 07:42:07 +0000

    Vesktop transparent background

--- config/hypr/hyprland/common/windowrule.lua
+++ config/hypr/hyprland/common/windowrule.lua
@@ -37,3 +37,11 @@ hl.window_rule({
 	center = true,
 	size = "(monitor_w*0.5) (monitor_h*0.5)",
 })
+
+hl.window_rule({
+  name = "vesktop_opacity",
+  match = {
+    class = "vesktop",
+  },
+  opacity = 0.9
+})
--- home/social/vesktop.nix
+++ home/social/vesktop.nix
@@ -1,3 +1,9 @@
 {
   programs.vesktop.enable = true;
+
+  stylix.targets.vesktop.colors.override = {
+    base0B = "497e68";
+    base0F = "11121d";
+  };
+
 }
