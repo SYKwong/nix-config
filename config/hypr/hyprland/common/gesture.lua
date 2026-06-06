30ea09efc3bbf907f21a159c10b5c2327f568a6c
Kyle Kwong <kylekwong1@gmail.com>
2026-06-06 08:34:14 +0000

    Hyprland: Fine tune config to match my preference

--- config/hypr/hyprland/common/gesture.lua
+++ config/hypr/hyprland/common/gesture.lua
@@ -1,5 +1,3 @@
-local utils = require("hyprland/common/utils")
-
 hl.gesture({
 	fingers = 4,
 	direction = "horizontal",
@@ -14,12 +12,6 @@ hl.gesture({
 
 hl.gesture({
 	fingers = 3,
-	direction = "left",
-	action = utils.cycle_window("next"),
-})
-
-hl.gesture({
-	fingers = 3,
-	direction = "right",
-	action = utils.cycle_window("prev"),
+	direction = "horizontal",
+	action = "scroll_move",
 })
--- config/hypr/hyprland/common/layout.lua
+++ config/hypr/hyprland/common/layout.lua
@@ -7,7 +7,7 @@ hl.config({
 	},
 	scrolling = {
 		fullscreen_on_one_column = false,
-		follow_min_visible = 0.0,
+		follow_min_visible = 0.4,
 		explicit_column_widths = "0.25, 0.5, 0.75, 1.0",
 	},
 })
