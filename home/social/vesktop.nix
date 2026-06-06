ed0b2eec4106bf0b1596ff4236bcff223f38855a
Kyle Kwong <kylekwong1@gmail.com>
2026-06-06 16:49:52 +0000

    Vesktop: use a custom theme over stylix

--- home/social/vesktop.nix
+++ home/social/vesktop.nix
@@ -1,11 +1,14 @@
 {
-  programs.vesktop.enable = true;
-
-  stylix.targets.vesktop.colors.override = {
-    base03 = "4c4f69";
-    base08 = "d20f39";
-    base0B = "497e68";
-    base0F = "11121d";
+  programs.vesktop = {
+    enable = true;
+    vencord = {
+      settings = {
+        useQuickCss = true;
+      };
+      extraQuickCss = ''
+        @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-mauve.theme.css");
+      '';
+    };
   };
-
+  stylix.targets.vesktop.enable = false;
 }
