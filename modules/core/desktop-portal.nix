dacfcb104af6d251126547cc515bc1766be333db
fw16-kyle <fw16-kyle@example.com>
2026-06-06 09:55:25 -0700

    clean up

--- modules/core/desktop-portal.nix
+++ modules/core/desktop-portal.nix
@@ -4,13 +4,11 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
       xdg-desktop-portal-gtk
     ];
 
     config.common.default = [ "gtk" ];
     config.gnome.default = [
-      "gnome"
       "gtk"
     ];
   };
