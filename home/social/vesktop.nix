8d2597fb961fe258b226cd8a877678317bb42e5d
fw16-kyle <fw16-kyle@example.com>
2026-06-08 21:10:13 -0700

    Vesktop: plugins

--- home/social/vesktop.nix
+++ home/social/vesktop.nix
@@ -4,6 +4,13 @@
     vencord = {
       settings = {
         useQuickCss = true;
+        plugins = {
+          MessageLogger = {
+            enabled = true;
+            ignoreSelf = true;
+          };
+          FakeNitro.enabled = true;
+        };
       };
       extraQuickCss = ''
         @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-mauve.theme.css");
