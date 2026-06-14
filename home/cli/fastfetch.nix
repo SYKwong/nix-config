e304d9c2b738400c1dd091156a2d9139e285d279
fw16-kyle <fw16-kyle@example.com>
2026-06-13 20:27:29 -0700

    fastfetch

--- home/cli/fastfetch.nix
+++ home/cli/fastfetch.nix
@@ -1,3 +1,118 @@
 {
-  programs.fastfetch.enable = true;
+  programs.fastfetch = {
+    enable = true;
+
+    settings = {
+      display = {
+        color = "35";
+        separator = " ➜  ";
+      };
+
+      modules = [
+        "break"
+        {
+          type = "os";
+          key = "OS   ";
+          keyColor = "31";
+        }
+        {
+          type = "kernel";
+          key = " ├  ";
+          keyColor = "31";
+        }
+        {
+          type = "packages";
+          key = " ├ 󰏖 ";
+          keyColor = "31";
+        }
+        {
+          type = "shell";
+          key = " └  ";
+          keyColor = "31";
+        }
+        "break"
+        {
+          type = "wm";
+          key = "WM   ";
+          keyColor = "32";
+        }
+        {
+          type = "wmtheme";
+          key = " ├ 󰉼 ";
+          keyColor = "32";
+        }
+        {
+          type = "icons";
+          key = " ├ 󰀻 ";
+          keyColor = "32";
+        }
+        {
+          type = "cursor";
+          key = " ├  ";
+          keyColor = "32";
+        }
+        {
+          type = "terminal";
+          key = " ├  ";
+          keyColor = "32";
+        }
+        {
+          type = "terminalfont";
+          key = " └  ";
+          keyColor = "32";
+        }
+        "break"
+        {
+          type = "host";
+          format = "{5} {1} Type {2}";
+          key = "PC   ";
+          keyColor = "33";
+        }
+        {
+          type = "cpu";
+          format = "{1} ({3}) @ {7}";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "gpu";
+          format = "{1} {2} @ {12} GHz";
+          key = " ├ 󰢮 ";
+          keyColor = "33";
+        }
+        {
+          type = "memory";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "disk";
+          key = " ├ 󰋊 ";
+          keyColor = "33";
+        }
+        {
+          type = "monitor";
+          key = " └  ";
+          keyColor = "33";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "{#94}System Statistics{#}";
+        }
+        {
+          type = "uptime";
+          key = "├ 󰾨 Uptime ";
+          keyColor = "94";
+        }
+        {
+          type = "command";
+          key = "└ 󰔟 OS Age ";
+          keyColor = "94";
+          text = "echo \"$(( ($(date +%s) - $(stat -c %W /)) / 86400 )) days\"";
+
+        }
+      ];
+    };
+  };
 }
