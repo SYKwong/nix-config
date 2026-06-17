e61e86e4c81ef46aa024269df08a629b39960168
fw16-kyle <fw16-kyle@example.com>
2026-06-17 16:22:28 -0700

    Hyprland: Set scrolling to fullscreen on one column

--- config/hypr/hyprland/common/layout.lua
+++ config/hypr/hyprland/common/layout.lua
@@ -6,7 +6,7 @@ hl.config({
 		new_status = "slave",
 	},
 	scrolling = {
-		fullscreen_on_one_column = false,
+		fullscreen_on_one_column = true,
 		follow_min_visible = 0.2,
 		explicit_column_widths = "0.25, 0.5, 0.75, 1.0",
 	},
