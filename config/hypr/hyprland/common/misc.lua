31a98908c0d7e89aed95633723df50bf477b69f6
fw16-kyle <fw16-kyle@example.com>
2026-06-19 09:07:06 -0700

    Hyprland: Config update

--- config/hypr/hyprland/common/misc.lua
+++ config/hypr/hyprland/common/misc.lua
@@ -2,7 +2,10 @@ hl.config({
 	misc = {
 		disable_hyprland_logo = true,
 		force_default_wallpaper = 0,
+		focus_on_activate = true,
 		disable_splash_rendering = true,
 		vrr = 1,
+		mouse_move_enables_dpms = true,
+		key_press_enables_dpms = true,
 	},
 })
