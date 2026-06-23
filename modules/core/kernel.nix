0b53517aaaa9030b70d5e4f5cf756c33ed0ff149
fw16-kyle <fw16-kyle@example.com>
2026-06-23 04:01:49 -0700

    switch to LTS kernel

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-zen4;
+  #boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-zen4;
   #boot.kernelPackages = pkgs.linuxPackages_latest;
 }
