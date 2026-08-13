7e559cc697702ab9e4f71e5a340bf0ba29fbfc0d
Kyle Kwong <kylekwong1@gmail.com>
2026-08-13 00:00:46 +0000

    chore(polkit): remove systemd user service from polkit-agenet.nix

--- modules/window-manager/common/polkit-agent.nix
+++ modules/window-manager/common/polkit-agent.nix
@@ -4,18 +4,4 @@
   environment.systemPackages = with pkgs; [
     hyprpolkitagent
   ];
-
-  systemd.user.services.hyprpolkitagent = {
-    description = "Hyprpolkit authentication agent";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
 }
