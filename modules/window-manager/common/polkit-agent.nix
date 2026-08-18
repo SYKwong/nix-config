9d2e3f1ab63f0abe91d62959b9d4b30726f80987
fw16-kyle <fw16-kyle@example.com>
2026-08-18 12:06:19 -0700

    fix: hyprpolkitagant autostart issue

--- modules/window-manager/common/polkit-agent.nix
+++ modules/window-manager/common/polkit-agent.nix
@@ -4,4 +4,13 @@
   environment.systemPackages = with pkgs; [
     hyprpolkitagent
   ];
+
+  systemd.user.services.hyprpolkitagent = {
+    description = "Hyprland Polkit Authentication Agent";
+    #wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
+      Restart = "on-failure";
+    };
+  };
 }
