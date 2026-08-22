bd5801f8aa41ab47744937e6a3bf3811d9c6130e
fw16-kyle <fw16-kyle@example.com>
2026-08-21 19:18:45 -0700

    feat(vpn): Add custom options for wireguard

--- hosts/framework16/wireguard.nix
+++ hosts/framework16/wireguard.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 {
+  custom.vpn.wireguard.enable = true;
   networking.wg-quick.interfaces.wg-home = {
     privateKeyFile = config.age.secrets.wireguard-fw16.path;
   };
--- modules/vpn/wireguard.nix
+++ modules/vpn/wireguard.nix
@@ -1,16 +1,29 @@
 {
-  networking.wg-quick.interfaces.wg-home = {
-    autostart = false;
+  config,
+  lib,
+  ...
+}:
 
-    address = [ "10.6.0.2/32" ];
-    dns = [ "10.6.0.1" ];
-    peers = [
-      {
-        publicKey = "HMNJm1ZJxA6nuAxzz8+ySbd0S4Mi1Su6ebrVf7wvRyw=";
-        allowedIPs = [ "0.0.0.0/0" ];
-        endpoint = "76.237.100.199:51820";
-        persistentKeepalive = 25;
-      }
-    ];
+let
+  cfg = config.custom.vpn.wireguard;
+in
+{
+  options.custom.vpn.wireguard.enable = lib.mkEnableOption "home WireGuard VPN";
+
+  config = lib.mkIf cfg.enable {
+    networking.wg-quick.interfaces.wg-home = {
+      autostart = false;
+
+      address = [ "10.6.0.2/32" ];
+      dns = [ "10.6.0.1" ];
+      peers = [
+        {
+          publicKey = "HMNJm1ZJxA6nuAxzz8+ySbd0S4Mi1Su6ebrVf7wvRyw=";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "76.237.100.199:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 }
