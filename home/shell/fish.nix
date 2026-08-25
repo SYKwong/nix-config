1104e2a4835b6bd6fc9c3a58c761411cc985e282
fw16-kyle <fw16-kyle@example.com>
2026-08-24 18:28:00 -0700

    feat(fish): Clean up unused namespaces, builtins. is no longer required

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -1,20 +1,12 @@
 {
-  lib,
-  config,
-  username,
-  ...
-}:
-
-{
   programs = {
     fish.enable = true;
-
     starship = {
       enable = true;
       enableFishIntegration = true;
 
       enableTransience = true;
-      settings = builtins.fromTOML (builtins.readFile ../../config/starship/starship.toml);
+      settings = fromTOML (builtins.readFile ../../config/starship/starship.toml);
     };
   };
   stylix.targets.starship.enable = false;
