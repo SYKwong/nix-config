572533539fb49d8e4952c894c9f17a658ea2936a
Kyle Kwong <kylekwong1@gmail.com>
2026-09-06 06:07:58 +0000

    Refactor/secrets dynamic ownership

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,11 +1,13 @@
+{ username, ... }:
+
 {
   age.secrets = {
     smb-credentials.file = ../../secrets/smb-credentials.age;
 
     cloudflare-workers-ai-apikey = {
       file = ../../secrets/cloudflare-workers-ai-apikey.age;
-      owner = "1000";
-      group = "100";
+      owner = username;
+      group = "users";
       mode = "0400";
     };
   };
