8d109c53ce9d1bf9f932e3328aa479219a9975d1
fw16-kyle <fw16-kyle@example.com>
2026-09-11 02:18:41 -0700

    feat(hyprland): enable layer animations for Noctalia

--- config/hypr/hyprland/common/layerrule.lua
+++ config/hypr/hyprland/common/layerrule.lua
@@ -3,7 +3,6 @@ hl.layer_rule({
 	match = {
 		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
 	},
-	no_anim = true,
 	ignore_alpha = 0.5,
 	blur = true,
 	blur_popups = true,
