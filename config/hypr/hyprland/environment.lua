8daecbcd258b7035520580a017b906e51ec6f2dc
k8-kyle <k8-kyle@example.com>
2026-09-15 02:03:24 -0700

    refactor(desktop): delegate cursor sizing to stylix and adjust size to 20

--- config/hypr/hyprland/environment.lua
+++ config/hypr/hyprland/environment.lua
@@ -1,7 +1,5 @@
 hl.env("GTK_CSD", "0")
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_MENU_PREFIX", "plasma-")
