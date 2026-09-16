d96b96701df7c65fcedbda217ba6eec7ca702c2b
k8-kyle <k8-kyle@example.com>
2026-09-16 04:01:16 -0700

    feat(mpv): enable autocreate playlist filter in system config

--- modules/core/system_packages.nix
+++ modules/core/system_packages.nix
@@ -1,38 +1,45 @@
 { pkgs, inputs, ... }:
 
 {
-  environment.systemPackages =
-    (with pkgs; [
-      brightnessctl
-      deadnix
-      ffmpeg
-      glow
-      jq
-      kitty
-      libnotify
-      mpv
-      p7zip
-      qimgv
-      qmk
-      qmk_hid
-      smartmontools
-      statix
-      wget
-      wl-clipboard
+  environment = {
+    systemPackages =
+      (with pkgs; [
+        brightnessctl
+        deadnix
+        ffmpeg
+        glow
+        jq
+        kitty
+        libnotify
+        mpv
+        p7zip
+        qimgv
+        qmk
+        qmk_hid
+        smartmontools
+        statix
+        wget
+        wl-clipboard
 
-      kdePackages.ark
-      kdePackages.dolphin
-      kdePackages.ffmpegthumbs
-      kdePackages.kio-extras
-    ])
-    ++ [
-      inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
+        kdePackages.ark
+        kdePackages.dolphin
+        kdePackages.ffmpegthumbs
+        kdePackages.kio-extras
+      ])
+      ++ [
+        inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+
+    etc."mpv/mpv.conf".text = ''
+      autocreate-playlist=filter
+    '';
+  };
 
   programs = {
     nano.enable = false;
     localsend.enable = true;
   };
+
   xdg = {
     terminal-exec = {
       enable = true;
