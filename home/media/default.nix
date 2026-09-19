c99dea2db5f6903a67c0bc70be4f195ee67cbec1
k8-kyle <k8-kyle@example.com>
2026-09-19 01:37:23 -0700

    feat(mpv): add mpris, seekbar thumbnails, silent arrow keys, and clean playlists

--- home/media/default.nix
+++ home/media/default.nix
@@ -9,14 +9,28 @@
 
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      thumbfast
+      thumbfast-vanilla-osc
+    ];
     config = {
+      # Disable built-in OSC so thumbfast-vanilla-osc provides the controller
+      osc = false;
+
+      # Do not show OSD pop-up/bar when scrubbing/seeking
+      osd-on-seek = "no";
+
       autocreate-playlist = "filter";
       directory-filter-types = "video";
+      directory-mode = "ignore";
       save-position-on-quit = true;
     };
     bindings = {
-      UP = "add volume 5";
-      DOWN = "add volume -5";
+      UP = "no-osd add volume 5";
+      DOWN = "no-osd add volume -5";
+      LEFT = "no-osd seek -5";
+      RIGHT = "no-osd seek 5";
     };
   };
 }
