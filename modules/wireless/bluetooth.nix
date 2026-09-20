8c22272c4a8dc3e358efddcdf8f87356020df552
k8-kyle <k8-kyle@example.com>
2026-09-20 00:07:24 -0700

    feat(wireless): enable bluetooth switch-on-connect and elevate bluez sink priority

--- modules/wireless/bluetooth.nix
+++ modules/wireless/bluetooth.nix
@@ -19,5 +19,34 @@ in
         };
       };
     };
+
+    services.pipewire = {
+      extraConfig.pipewire-pulse."99-switch-on-connect" = {
+        "pulse.cmd" = [
+          {
+            cmd = "load-module";
+            args = "module-switch-on-connect";
+          }
+        ];
+      };
+
+      wireplumber.extraConfig."10-bluetooth-policy" = {
+        "monitor.bluez.rules" = [
+          {
+            matches = [
+              {
+                "node.name" = "~bluez_output.*";
+              }
+            ];
+            actions = {
+              update-props = {
+                "priority.driver" = 1050;
+                "priority.session" = 1050;
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 }
